6695dbd58749e18b12bcfc6094a3dd0600b0a4a0
vallapuganesh597 <gv25aac@herts.ac.uk>
2025-12-12 18:44:11 +0530

    Update B 149.R

--- B 149.R
+++ B 149.R
@@ -28,3 +28,30 @@ airline_summary <- df %>%
 full_service <- c("Air India", "Vistara")
 df <- df %>%
   mutate(CarrierType = if_else(FlightName %in% full_service, "Full-service", "Low-cost"))
+full_prices <- df %>% filter(CarrierType == "Full-service") %>% pull(Price_int)
+low_prices  <- df %>% filter(CarrierType == "Low-cost")  %>% pull(Price_int)
+t_test_result <- t.test(full_prices, low_prices, var.equal = FALSE)
+print(t_test_result)
+
+p1 <- ggplot(airline_summary, aes(x = FlightName, y = mean_price)) +
+  geom_col() +
+  geom_errorbar(aes(ymin = mean_price - sd_price,
+                    ymax = mean_price + sd_price),
+                width = 0.2) +
+  labs(title = "Average Flight Price by Airline",
+       x = "Airline",
+       y = "Average Price (INR)") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+print(p1)
+
+p2 <- ggplot(df, aes(x = Price_int)) +
+  geom_histogram(binwidth = 500, boundary = 0, closed = "left") +
+  labs(title = "Distribution of Flight Prices (Delhi to Bangalore)",
+       x = "Price (INR)",
+       y = "Number of Flights") +
+  theme_minimal()
+
+
+print(p2)
